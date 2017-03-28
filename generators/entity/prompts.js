6e6ab5eb8c695f359c7208d9dc7ca90eaa2369c7
Michał Żmudziak <Micha mudziak>
2017-03-28 16:05:55 +0200

    Polish entity prompts and logging

--- generators/entity/prompts.js
+++ generators/entity/prompts.js
@@ -8,27 +8,36 @@ module.exports = {
 
 function askForEntity() {
     var done = this.async();
-    var prompts = [{
-        type: 'confirm',
-        name: 'generateAnotherEntity',
-        message: 'Would you like to generate another entity?',
-        default: true
-    }, {
-        when: (answer) => answer.generateAnotherEntity === true,
-        type: 'input',
-        name: 'entityName',
-        message: 'What name would you like for your entity?',
-        validate: function(input){
-            return input !== null && input !== '';
-        }
-    }];
+    var prompts = [
+        {
+            when: () => this.entities.length === 0,
+            type: 'confirm',
+            name: 'generateFirstEntity',
+            message: 'Would you like to generate an entity?',
+            default: true
+        },
+        {
+            when: () => this.entities.length > 0,
+            type: 'confirm',
+            name: 'generateAnotherEntity',
+            message: 'Would you like to generate another entity?',
+            default: true
+        }, {
+            when: (answer) => answer.generateFirstEntity === true || answer.generateAnotherEntity === true,
+            type: 'input',
+            name: 'entityName',
+            message: 'What name would you like for your entity?',
+            validate: function (input) {
+                return input !== null && input !== '';
+            }
+        }];
 
     this.prompt(prompts).then(function (answers) {
         var entity = {};
         entity.name = answers.entityName
-        this.entities.push(entity);
-        logEntities.call(this);
-        if (answers.generateAnotherEntity) {
+        if (answers.generateFirstEntity || answers.generateAnotherEntity) {
+            this.entities.push(entity);
+            logEntities.call(this);
             askForEntity.call(this, done);
         } else {
             done();
@@ -37,8 +46,10 @@ function askForEntity() {
 }
 
 function logEntities() {
-    this.log('Currently added entities:');
-    this.entities.forEach((element) => {
-        this.log(chalk.grey(element.name));
-    });
+    if (this.entities.length > 0) {
+        this.log('Currently added entities:');
+        this.entities.forEach((element) => {
+            this.log(chalk.grey(element.name));
+        });
+    }
 }
